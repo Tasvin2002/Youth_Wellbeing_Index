331934ff6d49a92878a9534b66c0bde692d8aa16
Tasvin2002 <tasvin0215@gmail.com>
2026-05-30 23:33:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ st.markdown("""
         margin-bottom: 0rem;
     }
     .q-hint {
-        font-size: 0.58rem;
+        font-size: 0.65rem !important;;
         color: #9896C8;
         margin-bottom: 0.1rem;
         margin-top: 0;
